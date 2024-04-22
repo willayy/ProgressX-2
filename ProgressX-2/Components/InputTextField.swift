a2637974347979d47eaec0f78384480391d00c97
William Norland <williamnorland@gmail.com>
2024-04-22 20:21:10 +0200

    Adding custom textfield for text input

--- ProgressX-2/Components/InputTextField.swift
+++ ProgressX-2/Components/InputTextField.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  InputTextField.swift
 //  ProgressX-2
 //
 //  Created by William Norland on 2024-04-20.
@@ -12,14 +12,14 @@ struct InputTextField: View {
     let placeHolder: String
     @Binding var text: String
     let width: CGFloat
-    private let foreGroundColor: Color = Color(red: 0.8, green: 0.8, blue: 0.8)
     
     var body: some View {
         TextField(placeHolder, text: $text)
             .frame(width: UIScreen.main.bounds.width * width)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
             .background(
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundColor(foreGroundColor)
+                RoundedRectangle(cornerRadius: 5)
+                    .foregroundColor(foreGroundColorGray)
                     .padding(.all, -3)
             )
             .onReceive(Just(text)) { newText in
