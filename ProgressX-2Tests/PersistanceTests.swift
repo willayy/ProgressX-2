3a3e35c6e2ae3996e0a5ad5a4a747029f9ab0b35
William Norland <williamnorland@gmail.com>
2024-04-22 20:24:35 +0200

    Adding a bunch of tests for the CoreData persistence.

--- ProgressX-2Tests/PersistanceTests.swift
+++ ProgressX-2Tests/PersistanceTests.swift
@@ -8,29 +8,80 @@
 import XCTest
 @testable import ProgressX_2
 
-final class ProgressX_2Tests: XCTestCase {
-
+final class PersistenceTests: XCTestCase {
+    
+    // MARK: SETUP
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let p = PersistenceController.shared
+        let today = Date()
+        let testHeight = 187.00
+        let testIsMetric = true
+        let testGender = "male"
+        let testWeight = 80.00
+        p.createProfile(userName: "TestProfile", birthDay: today, height: testHeight, isMetric: testIsMetric, gender: testGender)
+        p.addBodyWeightEntry(dateAchieved: today, weight: testWeight)
+        p.save()
     }
 
+    // MARK: TEAR DOWN
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        let p = PersistenceController.shared
+        p.wipeCoreDataBase()
+        p.save()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // MARK: TESTS
+    func testGetProfile() throws {
+        let p = PersistenceController.shared
+        var profile = p.getProfile()
+        XCTAssertNotNil(profile)
+        XCTAssertEqual(profile!.userName, "TestProfile")
+        p.deleteNSManagedObject(object: profile!)
+        p.save()
+        profile = p.getProfile()
+        XCTAssertNil(profile)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testGetProfileAsArray() throws {
+        let p = PersistenceController.shared
+        let profileArray = p.getProfileAsArray()
+        XCTAssertEqual(profileArray.count, 1)
+        XCTAssertEqual(profileArray.first!.userName, "TestProfile")
     }
-
+    
+    func testProfileState() throws {
+        let p = PersistenceController.shared
+        let profileState = p.verifyProfileState()
+        XCTAssertTrue(profileState)
+    }
+    
+    func testDoesProfileExist() throws {
+        let p = PersistenceController.shared
+        var doesTheProfileExist = p.doesProfileExist()
+        XCTAssertTrue(doesTheProfileExist)
+        let profile = p.getProfile()!
+        p.deleteNSManagedObject(object: profile)
+        p.save()
+        doesTheProfileExist = p.doesProfileExist()
+        XCTAssertFalse(doesTheProfileExist)
+    }
+    
+    func testGetBodyWeightEntriesAsArray() throws {
+        let p = PersistenceController.shared
+        var bodyEntries = p.getBodyWeightEntriesAsArray()
+        XCTAssertTrue(bodyEntries.count == 1)
+        var firstEntry = bodyEntries.first
+        XCTAssertEqual(firstEntry!.bodyWeight, 80.00)
+        
+        p.addBodyWeightEntry(dateAchieved: Date(), weight: 70.00)
+        p.save()
+        
+        bodyEntries = p.getBodyWeightEntriesAsArray()
+        XCTAssertTrue(bodyEntries.count == 2)
+        firstEntry = bodyEntries.first
+        let secondEntry = bodyEntries[1]
+        XCTAssertEqual(firstEntry!.bodyWeight, 80.00)
+        XCTAssertEqual(secondEntry.bodyWeight, 70.00)
+    }
+    
 }
