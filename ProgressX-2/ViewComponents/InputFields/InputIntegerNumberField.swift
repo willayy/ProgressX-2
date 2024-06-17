77577e58c5a0528d671e26f035912cda3cc53403
William Norland <williamnorland@gmail.com>
2024-06-18 00:15:17 +0200

    Changing folder names in view components

---
+++ ProgressX-2/ViewComponents/InputFields/InputIntegerNumberField.swift
@@ -0,0 +1,51 @@
+//
+//  InputIntegerNumberField.swift
+//  ProgressX-2
+//
+//  Created by William Norland on 2024-05-03.
+//
+
+
+import SwiftUI
+import Combine
+
+private let allowedChars = "1234567890"
+private let maxChars = 5
+
+/// TextField used for input of decimal numbers, using the InputField component.
+struct InputIntegerNumberField: View {
+    
+    let placeHolder: String
+    @Binding var numberText: String
+    @Binding var markAsWrong: Bool
+    let width: CGFloat
+    @State private var shouldShake = false
+    @Binding var errorMessage: String
+
+    private func onReceiveFunction(new: String) -> String {
+        // Filter out non allowed characters
+        var filtered = new.filter { allowedChars.contains($0) }
+        
+        if new.count > maxChars {
+            filtered.removeLast()
+        }
+        
+        return filtered
+    }
+    
+    private func onSubmitFunction(curr: String) -> String {
+        
+        if curr.isEmpty {
+            return "0"
+        }
+        
+        return curr
+    }
+    
+    var body: some View {
+        
+        InputField(value: $numberText, markAsWrong: $markAsWrong, errorMessage: errorMessage, placeHolder: placeHolder, width: width, onReceiveFunction: onReceiveFunction(new:), onSubmitFunction: onSubmitFunction(curr:))
+            .keyboardType(.numberPad)
+    }
+}
+
---
+++ ProgressX-2/ViewComponents/InputFields/InputShortTextField.swift
@@ -0,0 +1,39 @@
+//
+//  InputShortTextField.swift
+//  ProgressX-2
+//
+//  Created by William Norland on 2024-04-20.
+//
+
+import SwiftUI
+import Combine
+
+private let nonAllowedChars = ".,;´`'*^¨><()"
+private let maxChars = 25
+
+/// TextField used for short text input, composed of the base InputField component
+struct InputShortTextField: View {
+    
+    let placeHolder: String
+    @Binding var text: String
+    @Binding var markAsWrong: Bool
+    let width: CGFloat
+    @State private var shouldShake = false
+    @Binding var errorMessage: String
+
+    private func onReceiveFunction(new: String) -> String {
+        var filtered = new.filter { !nonAllowedChars.contains($0) }
+        if filtered.count > 25 {
+            filtered.removeLast()
+        }
+        return filtered
+    }
+    
+    private func onSubmitFunction(curr: String) -> String {
+        return curr
+    }
+    
+    var body: some View {
+        InputField(value: $text, markAsWrong: $markAsWrong, errorMessage: errorMessage, placeHolder: placeHolder, width: width, onReceiveFunction: onReceiveFunction(new:), onSubmitFunction: onSubmitFunction(curr:))
+    }
+}
