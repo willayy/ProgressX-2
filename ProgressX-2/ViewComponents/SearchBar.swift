0fcfd0aab00dc3394009203166b143439195345c
William Norland <williamnorland@gmail.com>
2024-06-20 00:11:46 +0200

    Created new routing system using enums

---
+++ ProgressX-2/ViewComponents/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  ProgressX-2
+//
+//  Created by William Norland on 2024-06-19.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
