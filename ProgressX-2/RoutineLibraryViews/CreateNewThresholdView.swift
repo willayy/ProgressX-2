89922402926a76579b7306393efa87a32298c829
William Norland <williamnorland@gmail.com>
2024-07-04 13:57:13 +0200

    Adding threshold views

---
+++ ProgressX-2/RoutineLibraryViews/CreateNewThresholdView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewThresholdView.swift
+//  ProgressX-2
+//
+//  Created by William Norland on 2024-07-04.
+//
+
+import SwiftUI
+
+struct CreateNewThresholdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewThresholdView()
+}
---
+++ ProgressX-2/RoutineLibraryViews/EditThresholdsView.swift
@@ -0,0 +1,18 @@
+//
+//  EditThresholdsView.swift
+//  ProgressX-2
+//
+//  Created by William Norland on 2024-07-04.
+//
+
+import SwiftUI
+
+struct EditThresholdsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditThresholdsView()
+}
