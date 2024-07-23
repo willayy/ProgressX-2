f2fac645e05858ef059233121f48d2e757d65948
William Norland <williamnorland@gmail.com>
2024-07-23 16:10:28 +0200

    Added rounding to computed loadTodo

--- ProgressX-2/DataModel/NSManagedObjectExtensions/TemplateTimePeriods/TemplateSet.swift
+++ ProgressX-2/DataModel/NSManagedObjectExtensions/TemplateTimePeriods/TemplateSet.swift
@@ -63,13 +63,21 @@ extension TemplateSet: HasOrderable {
                 exercise: exercise,
                 prType: prType
             )
+            // Compute the percentage
             let computedLoad: Double = (latestPr?.weightLoad ?? 0) * (self.setLoad / 100)
-            return computedLoad
+            // Round to smallest plate
+            let profile = PersistenceController.getProfile(self.managedObjectContext!)
+            let smallestPlate = profile!.smallestPlate * 2 // times two because you always add two weights for balance
+            let roundedLoad: Double = (computedLoad / smallestPlate).rounded() * smallestPlate
+            return roundedLoad
             
         case .bodyWeightPercentage:
             let latestBw = PersistenceController.getLatestBodyEntry(self.managedObjectContext!)
             let computedLoad: Double = (latestBw?.bodyWeight ?? 0) * (self.setLoad / 100)
-            return computedLoad
+            let profile = PersistenceController.getProfile(self.managedObjectContext!)
+            let smallestPlate = profile!.smallestPlate * 2 // times two because you always add two weights for balance
+            let roundedLoad: Double = (computedLoad / smallestPlate).rounded() * smallestPlate
+            return roundedLoad
         }
     }
     
