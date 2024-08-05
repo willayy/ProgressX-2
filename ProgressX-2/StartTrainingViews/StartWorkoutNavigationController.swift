c2809264ed449f4aeb8c135c17f8a1c3ea093f4c
William <williamnorland@gmail.com>
2024-08-05 17:46:17 +0200

    Removed the unnecessary bindings from the the views in the Navigationcontroller

--- ProgressX-2/StartTrainingViews/StartWorkoutNavigationController.swift
+++ ProgressX-2/StartTrainingViews/StartWorkoutNavigationController.swift
@@ -41,32 +41,28 @@ struct StartWorkoutNavigationController<Content: View>: View {
             }.navigationDestination(for: Int.self) { selection in
                 if selection == 1 {
                     
-                    // TODO: Create Training View
                     ChooseWeekView(
                         navPath: $navPath,
                         selectedRoutine: $selectedRoutine,
-                        selectedTrainingCycle: $selectedTrainingCycle,
-                        selectedTrainingWeek: $selectedTrainingWeek)
-                        .environment(\.managedObjectContext, viewContext)
+                        selectedTrainingWeek: $selectedTrainingWeek
+                    )
+                    .environment(\.managedObjectContext, viewContext)
                     
                 } else if selection == 2 {
                     
                     TrainingView(
                         navPath: $navPath,
-                        selectedRoutine: $selectedRoutine,
-                        selectedTrainingCycle: $selectedTrainingCycle,
-                        selectedTrainingWeek: $selectedTrainingWeek,
                         selectedTrainingSession: $selectedTrainingSession,
-                        AllTrainingSets: $AllTrainingSets,
-                        currentTrainingSet: $currentTrainingSet, Exercise: $exercise)
-                        .environment(\.managedObjectContext, viewContext)
-                    // TODO: Create choose week
+                        currentTrainingSet: $currentTrainingSet
+                    )
+                    .environment(\.managedObjectContext, viewContext)
                     
                 } else if selection == 3 {
                     
-                    // TODO: Create
-                    TrainingSetFinishedView(navPath: $navPath)
-                        .environment(\.managedObjectContext, viewContext)
+                    TrainingSetFinishedView(
+                        navPath: $navPath
+                    )
+                    .environment(\.managedObjectContext, viewContext)
                     
                 }
             }
