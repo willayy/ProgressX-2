9465f0e7fe82d583b8f3fb568d4a35d3e91ebf75
Lucas Häyhänen <lucas.hayhanen@live.se>
2024-08-26 14:19:20 +0200

    Creating and overlaping images

--- ProgressX-2/ExercisesViews/EditExerciseView/EditExerciseView.swift
+++ ProgressX-2/ExercisesViews/EditExerciseView/EditExerciseView.swift
@@ -107,7 +107,11 @@ struct EditExerciseView: View {
                     viewModel.selectedCategories.insert(category as! ExerciseCategory)
                 }
             })
+            //DisplayMusclesDummy(selectedMuscles: $viewModel.selectedCategories, categories: _categories)
+       
         }
+
+            
                 
         // MARK: Handle an edit of an exercise
         Button(action: {
